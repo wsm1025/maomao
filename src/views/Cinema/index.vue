a483cfccee7231cd064b625090169ecd553266dc
wsm1025 <taizongwang@126.com>
2020-07-22 19:36:35 +0800

    add createComponents

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="main">
+		<Header title="猫猫影院"></Header>
+		<div id="content">
+			<div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+			<Cinemalist></Cinemalist>
+		</div>
+		<Tabbar></Tabbar>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/header'
+	import Tabbar from '@/components/tabbar'
+	import Cinemalist from "@/components/CinemaList"
+	export default {
+		name: 'Cinema',
+		components: {
+			Header,
+			Tabbar,
+			Cinemalist
+		}
+	}
+</script>
+
+<style scoped>
+	#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="main">
+		<Header title="猫猫电影"></Header>
+		<div id="content">
+			<div class="movie_menu">
+							<router-link tag="div" to="/movie/city" class="city_name">
+								<span>大连</span><i class="iconfont icon-lower-triangle"></i>
+							</router-link>
+							<div class="hot_swtich">
+								<router-link tag="div" to="/movie/nowplaying" class="hot_item">正在热映</router-link>
+								<router-link tag="div" to="/movie/comingsoon" class="hot_item">即将上映</router-link>
+							</div>
+							<router-link tag="div" to="/movie/search"  class="search_entry">
+								<i class="iconfont icon-sousuo"></i>
+							</router-link>
+						</div>
+						<keep-alive>
+							<router-view></router-view>
+						</keep-alive>
+		</div>
+		<Tabbar></Tabbar>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/header'
+	import Tabbar from '@/components/tabbar'
+	export default{
+		name:'Movie',
+		components:{
+			Header,
+			Tabbar
+		}
+	}
+</script>
+
+<style scoped>
+	#content .movie_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-between; align-items:center; background:white; z-index:10;}
+	.movie_menu .city_name{ margin-left: 20px; height:100%; line-height: 45px;}
+	.movie_menu .hot_swtich{ display: flex; height:100%; line-height: 45px;}
+	.movie_menu .hot_item{ font-size: 15px; color:#666; width:80px; text-align:center; margin:0 12px; font-weight:700;}
+	.movie_menu .search_entry{ margin-right:20px; height:100%; line-height: 45px;}
+	.movie_menu .search_entry i{  font-size:24px; color:red;}
+	.router-link-active{color: #ef4238; border-bottom: 2px #ef4238 solid;}
+</style>
